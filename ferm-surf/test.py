09f583649fd4134df4a0660055c798c47c638e3c
Amy Searle <46977074+ajsearle97@users.noreply.github.com>
2019-07-17 11:40:55 -0700

    Add files via upload

---
+++ ferm-surf/test.py
@@ -0,0 +1,30 @@
+from interpolator import Interpolater
+from bulk_objects import FermiSurface, BrillouinZone, RecipCell
+from plotter import *
+
+from pymatgen.io.vasp.outputs import Vasprun
+from pymatgen.electronic_structure.core import Spin
+from pymatgen.electronic_structure.bandstructure import BandStructure
+
+if __name__ == '__main__':
+	vr = Vasprun("/Users/amyjade/URAP/PlottingProgram/dataMgB2/MgB2/vasprun.xml")
+	bs = vr.get_band_structure()
+
+	# increase interpolation factor to increase density of interpolated bandstructure
+	interpolater = Interpolater(bs) 
+
+	new_bs, hdims = interpolater.interpolate_bands(10)
+
+	fs = FermiSurface(new_bs, hdims)
+
+	bz = BrillouinZone(new_bs.lattice_rec._matrix)
+
+	rc = RecipCell(new_bs.lattice_rec._matrix)
+
+	plotter = FSPlotter(fs, bz, rc)
+
+	plotter.fs_plot_data(plot_type = 'plotly', title_str = r"Fermi surface of $Ca_{20}Ga_{8}N_{16}$")
+
+
+
+
