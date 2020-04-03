3aa06b4d3f62e999dc9f0593dfdf2fad75630b02
Alex Ganose <alexganose@googlemail.com>
2020-04-03 14:35:51 -0700

    Add integration tests

--- ifermi/tests/test_integration.py
+++ ifermi/tests/test_integration.py
@@ -1,43 +1,48 @@
+import unittest
 from pathlib import Path
 from ifermi.interpolator import Interpolater
 from ifermi.plotter import FSPlotter
 from ifermi.fermi_surface import FermiSurface
 from pymatgen import Spin
 from pymatgen.io.vasp.outputs import Vasprun
-import warnings
-warnings.simplefilter("ignore")
 
 example_dir = Path("../../examples")
-vr = Vasprun(example_dir / "MgB2/vasprun.xml")
-bs = vr.get_band_structure()
 
-interpolater = Interpolater(bs)
-new_bs, kpoint_dim = interpolater.interpolate_bands(5)
 
-fs = FermiSurface.from_band_structure(new_bs, kpoint_dim, wigner_seitz=False, spin=Spin.up)
-plotter = FSPlotter(fs)
-plotter.plot(plot_type='plotly')
+class IntegrationTest(unittest.TestCase):
 
-# Make a three dimensional plot of the Reciprocal Cell
+    def setUp(self):
+        vr = Vasprun(example_dir / "MgB2/vasprun.xml")
+        bs = vr.get_band_structure()
+        self.band_structure = bs
 
-# fs = FermiSurface(new_bs, hdims, rlattvec, mu = 0.0, plot_wigner_seitz = False)
+    def test_integration_wigner_seitz(self):
+        interpolater = Interpolater(self.band_structure)
+        new_bs, kpoint_dim = interpolater.interpolate_bands(1)
+        fs = FermiSurface.from_band_structure(new_bs, kpoint_dim)
+        plotter = FSPlotter(fs)
+        plotter.plot(plot_type='plotly', interactive=False)
+        plotter.plot(plot_type='mpl', interactive=False)
+        plotter.plot(plot_type='mayavi', interactive=False)
 
-# plotter = FSPlotter(fs, rc, bz = None)
+    def test_integration_reciprocal(self):
+        interpolater = Interpolater(self.band_structure)
+        new_bs, kpoint_dim = interpolater.interpolate_bands(1)
+        fs = FermiSurface.from_band_structure(new_bs, kpoint_dim, wigner_seitz=False)
+        plotter = FSPlotter(fs)
+        plotter.plot(plot_type='plotly', interactive=False)
+        plotter.plot(plot_type='mpl', interactive=False)
+        plotter.plot(plot_type='mayavi', interactive=False)
 
-# plotter.fs_plot_data(plot_type = 'mayavi')
+    def test_integration_spin(self):
+        interpolater = Interpolater(self.band_structure)
+        new_bs, kpoint_dim = interpolater.interpolate_bands(1)
+        fs = FermiSurface.from_band_structure(new_bs, kpoint_dim, spin=Spin.up)
+        plotter = FSPlotter(fs)
+        plotter.plot(plot_type='plotly', interactive=False)
+        plotter.plot(plot_type='mpl', interactive=False)
+        plotter.plot(plot_type='mayavi', interactive=False)
 
-# Same as above but using plotly package
-
-# fs = FermiSurface(new_bs, hdims, rlattvec, mu = 0.0, plot_wigner_seitz = False)
-
-# plotter = FSPlotter(fs, rc, bz = None)
-
-# plotter.fs_plot_data(plot_type = 'plotly')
-
-# Two dimesnional slice plot
-
-# fs = FermiSurface(new_bs, hdims, rlattvec, mu = 0.0, plot_wigner_seitz = False)
-
-# plotter = FSPlotter2D(fs, plane_orig = (0., 0., 0.0), plane_norm = (0, 0, 1))
-
-# plotter.fs2d_plot_data(plot_type = 'mpl')
+    def tearDown(self):
+        output_file = Path("fermi_surface.png")
+        output_file.unlink()
