b86ed599595888d0206ddd4c243806e9442defdf
Alex Ganose <alexganose@googlemail.com>
2020-04-05 00:42:17 -0700

    Add slice option to cli

--- ifermi/cli.py
+++ ifermi/cli.py
@@ -14,8 +14,9 @@ import os
 import sys
 import warnings
 from pathlib import Path
-from typing import Optional, Union
+from typing import Optional, Union, Tuple
 
+from ifermi.plotter import FermiSlicePlotter
 from pymatgen import Spin
 from pymatgen.io.vasp.outputs import Vasprun
 
@@ -34,6 +35,7 @@ def fsplot(
     spin: Optional[Spin] = None,
     plot_type: str = "plotly",
     interactive: bool = False,
+    slice_info: Optional[Tuple[float, float, float, float]] = None,
     prefix: Optional[str] = None,
     directory: Optional[Union[Path, str]] = None,
     image_format: str = "png",
@@ -52,6 +54,10 @@ def fsplot(
             "mayavi".
         interactive: Whether to enable interactive plots.
         prefix: Prefix for file names.
+        slice_info: Slice through the Brillouin zone. Given as the plane normal and
+            distance form the plane in fractional coordinates: E.g., ``[1, 0, 0, 0.2]``
+            where ``(1, 0, 0)`` are the miller indices and ``0.2`` is the distance from
+            the Gamma point.
         directory: The directory in which to save files.
         image_format: The image file format.
         dpi: The dots-per-inch (pixel density) for the image.
@@ -61,7 +67,7 @@ def fsplot(
     """
     from ifermi.fermi_surface import FermiSurface
     from ifermi.interpolator import Interpolater
-    from ifermi.plotter import FSPlotter
+    from ifermi.plotter import FermiSurfacePlotter
 
     if not filename:
         filename = find_vasprun_file()
@@ -76,18 +82,30 @@ def fsplot(
         interp_bs, kpoint_dim, mu=mu, wigner_seitz=wigner_seitz
     )
 
-    plotter = FSPlotter(fs)
-
     directory = directory if directory else "."
     prefix = "{}_".format(prefix) if prefix else ""
-    output_filename = "{}fermi_surface.{}".format(prefix, image_format)
-    output_filename = Path(directory) / output_filename
-    plotter.plot(
-        plot_type=plot_type,
-        interactive=interactive,
-        filename=output_filename,
-        spin=spin
-    )
+
+    if slice_info:
+        plane_normal = slice_info[:3]
+        distance = slice_info[3]
+
+        fermi_slice = fs.get_fermi_slice(plane_normal, distance)
+        plotter = FermiSlicePlotter(fermi_slice)
+
+        output_filename = "{}fermi_slice.{}".format(prefix, image_format)
+        output_filename = Path(directory) / output_filename
+        plotter.plot(filename=output_filename, spin=spin)
+    else:
+        plotter = FermiSurfacePlotter(fs)
+
+        output_filename = "{}fermi_surface.{}".format(prefix, image_format)
+        output_filename = Path(directory) / output_filename
+        plotter.plot(
+            plot_type=plot_type,
+            interactive=interactive,
+            filename=output_filename,
+            spin=spin
+        )
 
 
 def find_vasprun_file():
@@ -155,6 +173,13 @@ def _get_fs_parser():
         help="plotting type (options: mpl, plotly, mayavi)",
     )
     parser.add_argument(
+        "--slice",
+        type=float,
+        nargs=4,
+        metavar="N",
+        help="slice through the Brillouin zone (format: j k l distance)",
+    )
+    parser.add_argument(
         "--interpolate-factor",
         type=int,
         default=8,
@@ -200,108 +225,12 @@ def main():
         spin=args.spin,
         interactive=args.interactive,
         wigner_seitz=args.wigner_seitz,
+        slice_info=args.slice,
         prefix=args.prefix,
         directory=args.directory,
         image_format=args.image_format,
         dpi=args.dpi,
     )
-
-
-def fsplot2d(
-        filenames=None,
-        code="vasp",
-        prefix=None,
-        directory=None,
-        interpolate_factor=8,
-        mu=0.0,
-        image_format="pdf",
-        dpi=400,
-        plt=None,
-        fonts=None,
-        plane_orig=(0.0, 0.0, 0.5),
-        plane_norm=(0, 0, 1),
-):
-    """Plot electronic band structure diagrams from vasprun.xml files.
-
-    Args:
-        filenames (:obj:`str` or :obj:`list`, optional): Path to input files.
-            Vasp:
-                Use vasprun.xml or vasprun.xml.gz file.
-
-            If no filenames are provided, ifermi
-            will search for vasprun.xml or vasprun.xml.gz files in folders
-            named 'split-0*'. Failing that, the code will look for a vasprun in
-            the current directory.
-
-        code (:obj:`str`, optional): Calculation type. Default is 'vasp'
-        prefix (:obj:`str`, optional): Prefix for file names.
-        directory (:obj:`str`, optional): The directory in which to save files.
-        interpolate_factor (float, optional): The factor by which to interpolate the bands
-        mu (float, optional): The level above the Fermi energy at which the isosurfaces are to be
-            plotted.
-        image_format (:obj:`str`, optional): The image file format.
-        dpi (:obj:`int`, optional): The dots-per-inch (pixel density) for
-            the image.
-        plt (:obj:`matplotlib.pyplot`, optional): A
-            :obj:`matplotlib.pyplot` object to use for plotting.
-
-    Returns:
-        If ``plt`` set then the ``plt`` object will be returned. Otherwise, the
-        method will return a :obj:`list` of filenames written to disk.
-    """
-    from ifermi.fermi_surface import FermiSurface
-    from ifermi.interpolator import Interpolater
-    from ifermi.plotter import FSPlotter, FSPlotter2D
-
-    if not filenames:
-        filenames = find_vasprun_files()
-    elif isinstance(filenames, str):
-        filenames = [filenames]
-
-    # # don't save if pyplot object provided
-    # save_files = False if plt else True
-
-    vr = Vasprun(filenames)
-    bs = vr.get_band_structure()
-
-    if not interpolate:
-        interpolate_factor = 1
-
-    interpolater = Interpolater(bs)
-
-    new_bs, hdims, rlattvec = interpolater.interpolate_bands(interpolate_factor)
-
-    fs = FermiSurface(new_bs, hdims, rlattvec, mu=mu, plot_wigner_seitz=False)
-
-    if plot_wigner_seitz:
-
-        bz = WignerSeitzCell(rlattvec)
-        rc = None
-
-    else:
-
-        bz = None
-        rc = RecipCell(rlattvec)
-
-    plotter = FSPlotter2D(fs, plane_orig=plane_orig, plane_norm=plane_norm)
-
-    plotter.fs2d_plot_data(plot_type="mpl")
-
-    # if save_files:
-    #     basename = 'fs.{}'.format(image_format)
-    #     filename = '{}_{}'.format(prefix, basename) if prefix else basename
-    #     if directory:
-    #         filename = os.path.join(directory, filename)
-    #     plt.savefig(filename, format=image_format, dpi=dpi,
-    #                 bbox_inches='tight')
-    #
-    #     written = [filename]
-    #     written += save_data_files(bs, prefix=prefix,
-    #                                directory=directory)
-    #     return written
-    #
-    # else:
-    #     return plt
 
 
 def string_to_spin(spin_string):
