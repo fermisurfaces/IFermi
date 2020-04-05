e7bd1d76f84bfb2ff8c81a7b12f4506c6fe3381f
Alex Ganose <alexganose@googlemail.com>
2020-04-05 00:41:33 -0700

    Add fermi slice object

--- ifermi/fermi_surface.py
+++ ifermi/fermi_surface.py
@@ -3,48 +3,74 @@ This module contains the classes and methods for creating iso-surface structures
 from Pymatgen bandstrucutre objects. The iso-surfaces are found using the
 Scikit-image package.
 """
+
 import itertools
 import warnings
 from copy import deepcopy
 from typing import Dict, List, Tuple
 
 import numpy as np
+from dataclasses import dataclass
 from monty.json import MSONable
-from skimage import measure
 from skimage.measure import marching_cubes_lewiner
-from trimesh.intersections import slice_faces_plane
+from trimesh import Trimesh
+from trimesh.intersections import slice_faces_plane, mesh_plane, mesh_multiplane
 
-from ifermi.brillouin_zone import WignerSeitzCell, ReciprocalCell, ReciprocalSpace
+from ifermi.brillouin_zone import WignerSeitzCell, ReciprocalCell, ReciprocalSpace, \
+    ReciprocalSlice
 from pymatgen import Spin, Structure
 from pymatgen.electronic_structure.bandstructure import BandStructure
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
 
 
+@dataclass
+class FermiSlice(MSONable):
+
+    slices: Dict[Spin, List[np.ndarray]]
+    reciprocal_slice: ReciprocalSlice
+    structure: Structure
+
+    @classmethod
+    def from_dict(cls, d):
+        """Returns FermiSurface object from dict."""
+        fs = super().from_dict(d)
+        slices = {Spin(int(k)): v for k, v in fs.slices.items()}
+        return cls(slices, fs.reciprocal_slice, fs.structure)
+
+    def as_dict(self):
+        """
+        Get a json-serializable dict representation of FermiSurface.
+        """
+        d = {
+            "@module": self.__class__.__module__,
+            "@class": self.__class__.__name__,
+            "slices": {str(spin): iso for spin, iso in self.slices.items()},
+            "structure": self.structure.as_dict(),
+            "reciprocal_slice": self.reciprocal_slice.as_dict()
+        }
+        return d
+
+
+@dataclass
 class FermiSurface(MSONable):
     """An object containing Fermi Surface data.
 
     Only stores information at k-points where energy(k) == Fermi energy.
+
+    Args:
+        isosurfaces: A dictionary containing a list of isosurfaces as ``(vertices,
+            faces)`` for each spin channel.
+        reciprocal_space: The reciprocal space associated with the Fermi surface.
+        structure: The structure.
     """
 
-    def __init__(
-        self,
-        isosurfaces: Dict[Spin, List[Tuple[np.ndarray, np.ndarray]]],
-        reciprocal_space: ReciprocalSpace,
-        structure: Structure,
-    ):
-        """
-        Get a Fermi Surface object.
+    isosurfaces: Dict[Spin, List[Tuple[np.ndarray, np.ndarray]]]
+    reciprocal_space: ReciprocalSpace
+    structure: Structure
 
-        Args:
-            isosurfaces: A dictionary containing a list of isosurfaces as
-                ``(vertices, faces)`` for each spin channel.
-            reciprocal_space: The reciprocal space associated with the Fermi surface.
-            structure: The structure.
-        """
-        self.isosurfaces = isosurfaces
-        self.reciprocal_space = reciprocal_space
-        self.structure = structure
-        self.n_surfaces = len(self.isosurfaces)
+    @property
+    def n_surfaces(self):
+        return len(self.isosurfaces)
 
     @classmethod
     def from_band_structure(
@@ -109,20 +135,30 @@ class FermiSurface(MSONable):
 
         return cls(isosurfaces, reciprocal_space, structure)
 
-    def project_data(self, proj_plane: tuple):
-        projected_band = []
+    def get_fermi_slice(
+        self,
+        plane_normal: Tuple[int, int, int],
+        distance: float = 0
+    ) -> FermiSlice:
+        cart_normal = np.dot(plane_normal, self.reciprocal_space.reciprocal_lattice)
+        cart_origin = cart_normal * distance
 
-        for i, band in enumerate(self.isosurfaces):
-            verts = band[0]
-            faces = band[1]
-            projected_verts = []
+        slices = {}
+        for spin, spin_isosurfaces in self.isosurfaces.items():
+            spin_slices = []
 
-            for vertex in verts:
-                projected_verts.append(project(vertex, proj_plane))
+            for verts, faces in spin_isosurfaces:
+                mesh = Trimesh(vertices=verts, faces=faces)
+                lines = mesh_multiplane(mesh, cart_origin, cart_normal, [0])[0][0]
+                spin_slices.append(lines)
 
-            projected_band.append([projected_verts, faces])
+            slices[spin] = spin_slices
 
-        return projected_band
+        reciprocal_slice = self.reciprocal_space.get_reciprocal_slice(
+            plane_normal, distance
+        )
+
+        return FermiSlice(slices, reciprocal_slice, self.structure)
 
     @classmethod
     def from_dict(cls, d):
@@ -273,158 +309,3 @@ def get_prim_structure(structure, symprec=0.01) -> Structure:
     """
     analyzer = SpacegroupAnalyzer(structure, symprec=symprec)
     return analyzer.get_primitive_standard_structure()
-
-
-class FermiSurface2D(FermiSurface):
-    def __init__(
-        self,
-        bs: BandStructure,
-        hdims: list,
-        rlattvec,
-        slice_plane: tuple,
-        contour,
-        mu: float = 0.0,
-        soc: bool = False,
-    ) -> None:
-        """
-        Args:
-            bs (BandStructure): A Pymatgen bandstructure object
-            hdims (list): The dimension of the grid in reciprocal space on which the energy eigenvalues
-                are defined.
-            rlattvec (np.array): The reciprocal space lattice vectors. See
-                pymatgen.electronic_structure.bandstructure.lattice_rec._matrix for format.
-            slice_plane (tuple): The plane along which the surface is to be sliced. Only (0,0,1), (0,1,0)
-                or (1,0,0) are currently supported.
-            mu (float, optional): Enegy offset from the Fermi energy at which
-                the iso-surface is defined. Useful for visualising the effect of
-                dopants on the shape of the resulting iso-surface.
-            kpoints (np.array): A numpy list of the kpoints in fractional coordinates
-            soc (bool, optional): Set to True if the up and down spins are both to be plotted.
-                Otherwsie, spins will be treated as degenerate and only one componenet will be
-                plotted.
-            is_spin_polarised (bool, optional): set to True if spin polarised.
-            n_surfaces (int): Number of bands which cross the Fermi-Surface
-        """
-
-        self._mu = mu
-
-        self._fermi_level = bs.efermi + mu
-
-        self._kpoints = np.array([k.frac_coords for k in bs.kpoints])
-
-        self._hdims = hdims
-
-        dims = 2 * hdims + 1
-
-        self._dims = dims
-
-        self._k_dim = (dims[0], dims[1], dims[2])
-
-        self._rlattvec = rlattvec
-
-        self._slice_plane = slice_plane
-
-        self.slice_data(bs, self._slice_plane)
-
-        self.compute_isosurfaces(self._energies, self._fermi_level)
-
-        self._n_bands = len(self._iso_surface)
-
-        self._soc = soc
-
-        self._structure = bs.structure
-
-    def slice_data(self, bs, slice_plane: tuple):
-
-        for spin in self._bands.keys():
-
-            ebands = self._bands[spin]
-
-            plane_bands = []
-
-            dis_array = [
-                plane_dist(np.append(proj_plane, -contour), i) for i in self._kpoints
-            ]
-
-            for i, j in enumerate(slice_array):
-                if not j == 0:
-                    if i == 0:
-                        sort_indx = np.lexsort(dis_array[dis_array[:, 0].argsort()])
-                        plane_mesh = [1, mesh[1], mesh[2]]
-                    if i == 1:
-                        sort_indx = np.lexsort(dis_array[dis_array[:, 1].argsort()])
-                        plane_mesh = [mesh[0], 1, mesh[2]]
-
-                    if i == 2:
-                        sort_indx = np.lexsort(dis_array[dis_array[:, 2].argsort()])
-                        plane_mesh = [mesh[0], mesh[1], 1]
-
-            sorted_dist = dis_array(sort_index)
-            sorted_energies = ebands(sort_indx)
-            sorted_kpoints = self._kpoints(sort_index)
-
-            for dist, index in enumerate(sorted_dist):
-                while np.abs(dist - np.min(sorted_dist)) < 0.001:
-                    plane_bands.append(sorted_energies[index])
-
-            sort_idx = np.lexsort(
-                (sorted_kpoints[:, 2], sorted_kpoints[:, 1], sorted_kpoints[:, 0],)
-            )
-            energies_sorted = sorted_energies[sort_idx]
-
-            self._energies = energies.reshape(plane_mesh)
-
-    def compute_isosurfaces(self, energies, contour: float):
-
-        contours = measure.find_contours(energies, contour)
-
-        self._contours = contour
-
-
-def project(vector, plane):
-    theta = np.array([0, 0, np.pi])
-    a = np.array(
-        [
-            [1, 0, 0],
-            [0, np.cos(theta[0]), np.sin(theta[0])],
-            [0, -np.sin(theta[0]), np.cos(theta[0])],
-        ]
-    )
-
-    b = np.array(
-        [
-            [np.cos(theta[1]), 0, -np.sin(theta[1])],
-            [0, 1, 0],
-            [np.sin(theta[1]), 0, np.cos(theta[1])],
-        ]
-    )
-
-    c = np.array(
-        [
-            [np.cos(theta[2]), np.sin(theta[2]), 0],
-            [0, 1, 0],
-            [np.sin(theta[1]), 0, np.cos(theta[1])],
-        ]
-    )
-
-    d = np.matmul(np.matmul(a, np.matmul(b, c)), vector)
-
-    e = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
-
-    f = np.matmul(e, np.append(d, 1))
-
-    b_x = f[0] / f[3]
-    b_y = f[1] / f[3]
-
-    return [b_x, b_y, 0]
-
-
-def plane_dist(slice_plane, vertex):
-    return (
-        np.linalg.norm(
-            slice_plane[0] * vertex[0]
-            + slice_plane[1] * vertex[1]
-            + slice_plane[2] * vertex[2]
-            + slice_plane[3]
-        )
-    ) / (np.sqrt(slice_plane[0] ** 2 + slice_plane[1] ** 2 + slice_plane[2] ** 2))
