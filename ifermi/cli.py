a981c1fe254a4e23f7b3d9e6089a1d2dca7c0bbd
Alex Ganose <alexganose@googlemail.com>
2021-02-09 19:56:13 -0800

    Rewrite cli using click

--- ifermi/cli.py
+++ ifermi/cli.py
@@ -2,80 +2,118 @@
 # Distributed under the terms of the MIT License.
 
 """
-A script to plot Fermi surfaces
+This modules defines command line tools for generating and plotting Fermi surfaces.
 """
 
-
-import argparse
 import os
 import sys
 import warnings
-from pathlib import Path
-from typing import Optional, Tuple, Union
 
-__author__ = "Amy Searle"
+import click
+from click import option
+
+from ifermi.defaults import AZIMUTH, ELEVATION, SCALE, SYMPREC, VECTOR_SPACING
+
+__author__ = "Amy Searle, Alex Ganose"
 __version__ = "0.1.0"
-__maintainer__ = "Amy Searle"
+__maintainer__ = "Amy Searle, Alex Ganose"
 __email__ = "amyjadesearle@gmail.com"
-__date__ = "Sept 18, 2019"
+__date__ = "Feb 09, 2021"
+
+plot_type = click.Choice(["matplotlib", "plotly", "mayavi"], case_sensitive=False)
+spin_type = click.Choice(["up", "down"], case_sensitive=False)
+projection_type = click.Choice(["velocity"], case_sensitive=False)
 
 
-def ifermi(
-    filename: Optional[Union[Path, str]] = None,
-    interpolation_factor: int = 8,
-    decimate_factor: Optional[float] = None,
-    mu: float = 0.0,
-    azimuth: float = 45.0,
-    elevation: float = 35.0,
-    wigner_seitz: bool = True,
-    spin: Optional["Spin"] = None,
-    smooth: bool = False,
-    projection: Optional[str] = None,
-    color_projection: bool = True,
-    vector_projection: bool = False,
-    hide_surface: bool = False,
-    plot_type: str = "plotly",
-    slice_info: Optional[Tuple[float, float, float, float]] = None,
-    output_filename: Optional[str] = None,
-    scale: float = 4,
-):
-    """Plot Fermi surfaces from a vasprun.xml file.
-
-    Args:
-        filename: Path to input vasprun file.
-        interpolation_factor: The factor by which to interpolate the bands.
-        output_filename: The output file name. This will prevent the plot from being
-            interactive.
-        decimate_factor: Scaling factor by which to reduce the number of faces.
-        mu: The level above the Fermi energy at which the isosurfaces are to be plotted.
-        azimuth: The azimuth of the viewpoint in degrees. i.e. the angle subtended
-            by the position vector on a sphere projected on to the x-y plane.
-        elevation: The zenith angle of the viewpoint in degrees, i.e. the angle
-            subtended by the position vector and the z-axis.
-        wigner_seitz: Controls whether the cell is the Wigner-Seitz cell or the
-            reciprocal unit cell parallelepiped.
-        spin: The spin channel to plot. By default plots both spin channels. Should be
-            a pymatgen ``Spin`` object.
-        smooth: If True, will smooth FermiSurface. Requires PyMCubes. See
-            ``compute_isosurfaces`` for more information.
-        projection: Projection type, can be "velocity", "spin" or None.
-        color_projection: Whether to use the projections to color the Fermi surface. If
-            the projections is a vector then the norm of the projections will be used.
-        vector_projection: Whether to plot arrows for vector projections.
-        hide_surface: Whether to hide the Fermi surface. Only recommended in combination
-            with the vector_projection option.
-        plot_type: Method used for plotting. Valid options are: "matplotlib", "plotly",
-            "mayavi".
-        slice_info: Slice through the Brillouin zone. Given as the plane normal and
-            distance form the plane in fractional coordinates: E.g., ``[1, 0, 0, 0.2]``
-            where ``(1, 0, 0)`` are the miller indices and ``0.2`` is the distance from
-            the Gamma point.
-        scale: Scale for the figure size. Increases resolution but does not change the
-            relative size of the figure and text.
-
-    Returns:
-        The filename written to disk.
+@click.group(
+    context_settings=dict(help_option_names=["-h", "--help"], max_content_width=120)
+)
+def cli():
     """
+    ifermi is a tool for the generation, analysis and plotting of Fermi surfaces
+    """
+
+    def _warning(message, *_, **__):
+        click.echo("WARNING: {}\n".format(message))
+
+    warnings.showwarning = _warning
+
+    warnings.filterwarnings("ignore", category=UserWarning, module="matplotlib")
+    warnings.filterwarnings("ignore", category=UnicodeWarning, module="matplotlib")
+    warnings.filterwarnings("ignore", category=UserWarning, module="pymatgen")
+
+
+@cli.command(context_settings=dict(show_default=True))
+@option("-f", "--filename", help="vasprun.xml file to plot")
+@option("-o", "--output", "output_filename", help="output filename")
+@option(
+    "-m",
+    "--mu",
+    default=0.0,
+    help="offset from the Fermi level at which to calculate Fermi surface",
+)
+@option(
+    "-i",
+    "--interpolation-factor",
+    default=8.0,
+    help="interpolation factor for band structure",
+)
+@option(
+    "--wigner/--no-wigner",
+    "wigner_seitz",
+    default=True,
+    help="use Wigner-Seitz cell rather than reciprocal lattice parallelepiped",
+)
+@option("-s", "--symprec", default=SYMPREC, help="symmetry precision in Å")
+@option("-a", "--azimuth", default=AZIMUTH, help="viewpoint azimuth angle in °")
+@option("-e", "--elevation", default=ELEVATION, help="viewpoint elevation angle in °")
+@option(
+    "-t", "--type", "plot_type", default="plotly", type=plot_type, help="plotting type"
+)
+@option("--projection", type=projection_type, help="projection type")
+@option(
+    "--color-projection/--no-color-projection",
+    default=True,
+    help="color Fermi surface projections",
+)
+@option("--projection-colormap", help="matplotlib colormap name for projections")
+@option(
+    "--vector-projection/--no-vector-projection",
+    help="show vector projections as arrows",
+)
+@option("--vector-colormap", help="matplotlib colormap name for vectors")
+@option(
+    "--projection-axis",
+    nargs=3,
+    type=float,
+    help="color projection by projecting onto cartesian axis (e.g. 0 0 1)",
+)
+@option(
+    "--vector-spacing", default=VECTOR_SPACING, help="spacing between projection arrows"
+)
+@option("--cmin", type=float, help="minimum intensity on projection colorbar")
+@option("--cmax", type=float, help="maximum intensity on projection colorbar")
+@option("--vnorm", type=float, help="value by which to normalise vector lengths")
+@option("--hide-surface", is_flag=True, help="hide the Fermi surface")
+@option("--spin", type=spin_type, help="select spin channel")
+@option("--smooth", is_flag=True, help="smooth the Fermi surface")
+@option(
+    "--slice",
+    nargs=4,
+    type=float,
+    help="slice through the Brillouin zone (format: j k l dist)",
+)
+@option(
+    "--decimate-factor",
+    type=float,
+    help="factor by which to decimate surfaces (i.e. 0.8 gives 20 %% fewer faces)",
+)
+@option("--scale", default=SCALE, help="scale for image resolution")
+def plot(filename, **kwargs):
+    """
+    Plot Fermi surfaces from a vasprun.xml file.
+    """
+    from pymatgen.electronic_structure.core import Spin
     from pymatgen.io.vasp.outputs import Vasprun
 
     from ifermi.fermi_surface import FermiSurface
@@ -93,7 +131,14 @@ def ifermi(
     except ImportError:
         mlab = False
 
-    if mlab and plot_type == "mayavi" and output_filename is not None:
+    if kwargs["projection_colormap"]:
+        kwargs["color_projection"] = kwargs["projection_colormap"]
+
+    if kwargs["vector_colormap"]:
+        kwargs["vector_projection"] = kwargs["vector_colormap"]
+
+    output_filename = kwargs["output_filename"]
+    if mlab and kwargs["plot_type"] == "mayavi" and output_filename is not None:
         # handle mlab non interactive plots
         mlab.options.offscreen = True
 
@@ -104,51 +149,62 @@ def ifermi(
     bs = vr.get_band_structure()
 
     interpolator = Interpolator(bs)
+    interp_bs, velocities = interpolator.interpolate_bands(
+        kwargs["interpolation_factor"], return_velocities=True
+    )
 
-    if projection == "velocity":
-        interp_bs, projection_data = interpolator.interpolate_bands(
-            interpolation_factor, return_velocities=True
-        )
+    projection_data = None
+    projection_kpoints = None
+    if kwargs["projection"] == "velocity":
+        projection_data = velocities
         projection_kpoints = get_kpoints_from_bandstructure(interp_bs)
-    else:
-        interp_bs = interpolator.interpolate_bands(interpolation_factor)
-        projection_data = None
-        projection_kpoints = None
+    elif kwargs["projection"] is not None:
+        click.echo("unrecognised projection type - valid options are: velocity")
+        sys.exit()
 
     fs = FermiSurface.from_band_structure(
         interp_bs,
-        mu=mu,
-        wigner_seitz=wigner_seitz,
-        decimate_factor=decimate_factor,
-        smooth=smooth,
+        mu=kwargs["mu"],
+        wigner_seitz=kwargs["wigner_seitz"],
+        decimate_factor=kwargs["decimate_factor"],
+        smooth=kwargs["smooth"],
         projection_data=projection_data,
         projection_kpoints=projection_kpoints,
     )
-    if slice_info:
-        plane_normal = slice_info[:3]
-        distance = slice_info[3]
+
+    spin = {"up": Spin.up, "down": Spin.down, None: None}[kwargs["spin"]]
+
+    if kwargs["slice"]:
+        plane_normal = kwargs["slice"][:3]
+        distance = kwargs["slice"][3]
 
         fermi_slice = fs.get_fermi_slice(plane_normal, distance)
-        plotter = FermiSlicePlotter(fermi_slice)
-
-        plot = plotter.get_plot(spin)
+        plotter = FermiSlicePlotter(fermi_slice, symprec=kwargs["symprec"])
+        fig = plotter.get_plot(spin)
     else:
-        plotter = FermiSurfacePlotter(fs)
-        plot = plotter.get_plot(
-            plot_type=plot_type,
+        plotter = FermiSurfacePlotter(fs, symprec=kwargs["symprec"])
+        projection_axis = kwargs["projection_axis"] or None
+
+        fig = plotter.get_plot(
+            plot_type=kwargs["plot_type"],
             spin=spin,
-            azimuth=azimuth,
-            elevation=elevation,
-            color_projection=color_projection,
-            vector_projection=vector_projection,
-            hide_surface=hide_surface,
+            azimuth=kwargs["azimuth"],
+            elevation=kwargs["elevation"],
+            color_projection=kwargs["color_projection"],
+            vector_projection=kwargs["vector_projection"],
+            projection_axis=projection_axis,
+            vector_spacing=kwargs["vector_spacing"],
+            cmin=kwargs["cmin"],
+            cmax=kwargs["cmax"],
+            vnorm=kwargs["vnorm"],
+            hide_surface=kwargs["hide_surface"],
         )
 
     if output_filename is None:
-        show_plot(plot)
+        show_plot(fig)
     else:
-        print("Saving plot to {}".format(output_filename))
-        save_plot(plot, output_filename, scale=scale)
+        click.echo("Saving plot to {}".format(output_filename))
+        save_plot(fig, output_filename, scale=kwargs["scale"])
 
 
 def find_vasprun_file():
@@ -160,145 +216,5 @@ def find_vasprun_file():
         if os.path.exists(file):
             return file
 
-    print("ERROR: No vasprun.xml found in current directory")
+    click.echo("ERROR: No vasprun.xml found in current directory")
     sys.exit()
-
-
-def _get_fs_parser():
-    parser = argparse.ArgumentParser(
-        description="""
-    ifermi is a package for plotting Fermi-surfaces""",
-        epilog="""
-    Author: {}
-    Version: {}
-    Last updated: {}""".format(
-            __author__, __version__, __date__
-        ),
-    )
-
-    parser.add_argument(
-        "-f", "--filename", default=None, metavar="F", help="vasprun.xml file to plot"
-    )
-    parser.add_argument(
-        "-o", "--output", dest="output_filename", metavar="O", help="output filename"
-    )
-    parser.add_argument(
-        "-m",
-        "--mu",
-        default=0.0,
-        type=float,
-        help="offset from the Fermi level at which to calculate Fermi surface",
-    )
-    parser.add_argument(
-        "-i",
-        "--interpolation-factor",
-        type=int,
-        default=8,
-        dest="interpolation_factor",
-        metavar="N",
-        help="interpolation factor for band structure (default: 8)",
-    )
-    parser.add_argument(
-        "-r",
-        "--reciprocal-cell",
-        dest="wigner_seitz",
-        action="store_false",
-        help="use the reciprocal lattice rather than Wigner-Seitz cell",
-    )
-    parser.add_argument(
-        "--spin",
-        type=string_to_spin,
-        default=None,
-        help="select spin channel (options: up, 1; down, -1)",
-    )
-    parser.add_argument(
-        "-a",
-        "--azimuth",
-        type=float,
-        default=45.0,
-        metavar="A",
-        help="viewpoint azmith angle in degrees (default: 45)",
-    )
-    parser.add_argument(
-        "-e",
-        "--elevation",
-        type=float,
-        default=35.0,
-        metavar="E",
-        help="viewpoint elevation (zenith) angle in degrees (default: 35)",
-    )
-    parser.add_argument(
-        "--smooth", action="store_true", help="smooth the Fermi surface"
-    )
-    parser.add_argument(
-        "-t",
-        "--type",
-        dest="plot_type",
-        default="plotly",
-        help="plotting type (options: matplotlib, plotly, mayavi)",
-    )
-    parser.add_argument(
-        "--slice",
-        type=float,
-        nargs=4,
-        metavar="N",
-        help="slice through the Brillouin zone (format: j k l distance)",
-    )
-    parser.add_argument(
-        "--decimate-factor",
-        type=float,
-        default=None,
-        dest="decimate_factor",
-        metavar="N",
-        help="factor by which to decimate Fermi surfaces (i.e., 0.8 gives 20 %% fewer "
-        "faces)",
-    )
-    parser.add_argument(
-        "--scale", type=float, default=4, help="scale for image resolution (default: 4)"
-    )
-    return parser
-
-
-def main():
-    args = _get_fs_parser().parse_args()
-
-    warnings.filterwarnings("ignore", category=UserWarning, module="matplotlib")
-    warnings.filterwarnings("ignore", category=UnicodeWarning, module="matplotlib")
-    warnings.filterwarnings("ignore", category=UserWarning, module="pymatgen")
-
-    ifermi(
-        filename=args.filename,
-        interpolation_factor=args.interpolation_factor,
-        decimate_factor=args.decimate_factor,
-        mu=args.mu,
-        plot_type=args.plot_type,
-        azimuth=args.azimuth,
-        elevation=args.elevation,
-        spin=args.spin,
-        smooth=args.smooth,
-        wigner_seitz=args.wigner_seitz,
-        slice_info=args.slice,
-        output_filename=args.output_filename,
-        scale=args.scale,
-    )
-
-
-def string_to_spin(spin_string):
-    """Function to convert 'spin' cli argument to pymatgen Spin object"""
-    from pymatgen import Spin
-
-    if spin_string in ["up", "Up", "1", "+1"]:
-        return Spin.up
-
-    elif spin_string in ["down", "Down", "-1"]:
-        return Spin.down
-
-    elif spin_string is None:
-        return None
-
-    else:
-        raise ValueError("Unable to parse 'spin' argument")
-
-
-if __name__ == "__main__":
-    main()
