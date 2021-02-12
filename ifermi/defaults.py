333514e345c0a8de4a3b23e6c5e5ba98f2ea41b8
Alex Ganose <alexganose@googlemail.com>
2021-02-12 15:00:47 -0800

    Update defaults

--- ifermi/defaults.py
+++ ifermi/defaults.py
@@ -1,6 +1,10 @@
+"""This module defines common defaults."""
+
 AZIMUTH = 45.0
 ELEVATION = 35.0
 VECTOR_SPACING = 0.2
 COLORMAP = "viridis"
 SYMPREC = 1e-3
+KTOL = 1e-5
 SCALE = 4
+PROJECTION_INTERPOLATION_FACTOR = 10
