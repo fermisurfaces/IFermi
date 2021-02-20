Fix velocity units

ifermi/analysis.py
@@ -65,7 +65,7 @@ def average_properties(
     face_areas = mesh.area_faces
 
     # face_areas has to have same number of dimensions as property
-    face_areas = face_areas.reshape(face_areas.shape + (1, ) * (properties.ndim - 1))
+    face_areas = face_areas.reshape(face_areas.shape + (1,) * (properties.ndim - 1))
 
     return np.sum(properties * face_areas, axis=0) / mesh.area
 

ifermi/cli.py
@@ -1,9 +1,8 @@
 """Command line tools for generating and plotting Fermi surfaces."""
-from collections import defaultdict
-
 import os
 import sys
 import warnings
+from collections import defaultdict
 
 import click
 from click import option
@@ -88,14 +87,14 @@ def cli():
 )
 @option(
     "--precision",
-    default=3,
+    default=4,
     help="number of decimal places in output",
     show_default=True,
 )
 def info(filename, **kwargs):
     """Calculate information about the Fermi surface."""
-    from tabulate import tabulate
     import numpy as np
+    from tabulate import tabulate
 
     fs, bs = _get_fermi_surface(
         filename=filename,
@@ -114,10 +113,7 @@ def info(filename, **kwargs):
         if isinstance(value, (list, tuple, np.ndarray)):
             return str(np.array(value))
         else:
-            if value < 10 ** (-kwargs["precision"] + 1):
-                return f"{value:.{kwargs['precision']}g}"
-            else:
-                return f"{value:.{kwargs['precision']}f}"
+            return f"{value:.{kwargs['precision']}g}"
 
     norm = kwargs["norm"]
     axis = kwargs["projection_axis"] or None
@@ -167,7 +163,7 @@ def info(filename, **kwargs):
             headers=table.keys(),
             numalign="right",
             stralign="center",
-            floatfmt=f".{kwargs['precision']}f",
+            floatfmt=f"#.{kwargs['precision']}g",
         )
 
         # indent table 2 spaces in
@@ -260,12 +256,7 @@ def plot(filename, **kwargs):
     """Plot a Fermi surface from a vasprun.xml file."""
     from pymatgen.electronic_structure.core import Spin
 
-    from ifermi.plot import (
-        FermiSlicePlotter,
-        FermiSurfacePlotter,
-        save_plot,
-        show_plot,
-    )
+    from ifermi.plot import FermiSlicePlotter, FermiSurfacePlotter, save_plot, show_plot
 
     try:
         import mayavi.mlab as mlab

ifermi/interpolate.py
@@ -71,10 +71,10 @@ class FourierInterpolator:
         """
         import multiprocessing
 
-        from scipy.constants import physical_constants
         from BoltzTraP2 import fite, sphere
         from BoltzTraP2.units import eV
         from pymatgen.io.ase import AseAtomsAdaptor
+        from scipy.constants import physical_constants
         from spglib import spglib
 
         from ifermi.boltztrap import bands_fft
@@ -114,10 +114,10 @@ class FourierInterpolator:
             energies[spin] /= eV
 
             # velocities in Bohr radius * Rydberg / hbar, convert to m/s.
-            # velocities[spin] *= (
-            #     physical_constants["Bohr radius"][0] /
-            #     physical_constants["atomic unit of time"][0]
-            # )
+            velocities[spin] *= (
+                physical_constants["Bohr radius"][0]
+                / physical_constants["atomic unit of time"][0]
+            )
 
         efermi = self._band_structure.efermi
 

ifermi/tests/test_fermi_surface.py
@@ -58,7 +58,9 @@ class FermiSurfaceTest(unittest.TestCase):
 
             for s1, s2 in zip(iso1, iso2):
                 # check vertex coordinates are almost equal
-                np.testing.assert_array_almost_equal(s1.vertices, s2.vertices, decimal=5)
+                np.testing.assert_array_almost_equal(
+                    s1.vertices, s2.vertices, decimal=5
+                )
 
                 # check faces are exactly equal
                 np.testing.assert_array_equal(s1.faces, s2.faces)