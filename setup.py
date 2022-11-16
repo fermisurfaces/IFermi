c7318f411336b97c1eddb7c88c40a5f8895595f4
Alex Ganose <utf@users.noreply.github.com>
2022-11-16 09:31:30 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -61,9 +61,9 @@ setup(
         "docs": [
             "sphinx==5.3.0",
             "sphinx-click==4.3.0",
-            "sphinx_rtd_theme==1.1.1",
+            "sphinx_rtd_theme==1.0.0",
             "sphinx-autodoc-typehints==1.19.4",
-            "m2r2==0.3.3",
+            "m2r2==0.3.2",
             "nbsphinx",
             "nbsphinx-link",
             "ipython",
